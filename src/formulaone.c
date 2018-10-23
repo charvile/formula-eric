2d438239e7463df4748a3d2b3d692db15fe14b74
Andre Debuisne <andre.debuisne@epita.fr>
2018-10-23 18:21:49 +0200

    Optimized granny

--- src/formulaone.c
+++ src/formulaone.c
@@ -12,8 +12,7 @@ int is_on_starting_point(struct car *car)
     return 0;
 }
 
-
-static int is_facing_road(struct car *car)
+static int is_facing_obj(struct car *car, enum floortype obj, int distance)
 {
     //fprintf(stderr, "Car angle is : %f ; %f\n", car->direction.x, car->direction.y);
     int pos_x = car->position.x;
@@ -24,26 +23,17 @@ static int is_facing_road(struct car *car)
 
     //fprintf(stderr, "New car angle is : %d ; %d\n", angle_x, angle_y);
 
-    if (map_get_floor(car->map, pos_x + angle_x, pos_y + angle_y) != ROAD)
+    if (map_get_floor(car->map, pos_x + angle_x * distance,
+        pos_y + angle_y * distance) != obj)
     {
         return 0;
     }
     return 1;
 }
 
-static int is_facing_road_long_term(struct car *car)
+static int is_at_desired_angle(struct car *car, float angle)
 {
-    int pos_x = car->position.x;
-    int pos_y = car->position.y;
 
-    int angle_x = floor(car->direction.x + CAR_TURN_ANGLE);
-    int angle_y = floor(car->direction.y + CAR_TURN_ANGLE);
-
-    if (map_get_floor(car->map, pos_x + angle_x * 2, pos_y + angle_y * 2) != ROAD)
-    {
-        return 0;
-    }
-    return 1;
 }
 
 enum move update(struct car *car)
@@ -52,15 +42,15 @@ enum move update(struct car *car)
     {
         map = map_init(car->map);
     }
-    if (is_facing_road(car) && is_facing_road_long_term(car))
+    if (is_facing_obj(car, ROAD, 1) && is_facing_obj(car, ROAD, 2))
     {
         //if (car->speed.x > 0.05f || car->speed.y > 0.05f)
         //{
             //return BRAKE;
         //}
-        return ACCELERATE;
+        //return ACCELERATE;
     }
-    else if (is_facing_road(car) && !is_facing_road_long_term(car))
+    else if (is_facing_obj(car, ROAD, 1) && !is_facing_obj(car, ROAD, 2))
     {
         return TURN_RIGHT;
     }
