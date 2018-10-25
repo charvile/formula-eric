9e0d220e5782276ca21b5d888db8dabe376fc9f8
Andre Debuisne <andre.debuisne@gmail.com>
2018-10-25 18:40:42 +0200

    Modified node structure

--- src/path_finder.h
+++ src/path_finder.h
@@ -7,8 +7,8 @@ struct node
 {
     char type;
     char open; /* 0 -> not seen | 1 -> closed | 2 -> open */
-    struct vector2 next; /* Position in matrix of following node*/
-    struct vector2 previous; /* Position in matrix of previous node*/
+    struct node *next; /* Position in matrix of following node*/
+    struct node *previous; /* Position in matrix of previous node*/
     struct vector2 pos; /* Position on the map*/
     int i; /* Indexes in the matrix */ 
     int j;
--- src/path_finder_2.c
+++ src/path_finder_2.c
@@ -2,5 +2,10 @@
 
 void draw_best_path(struct node start, struct node finish)
 {
+    struct node current = finish;
 
+    while(current.pos.x != start.pos.x && current.pos.y != start.pos.y)
+    {
+        current = g_map[current.previous->j][current.previous->j]; 
+    }
 }
