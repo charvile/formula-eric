7e9c995f7e9f32ec38d37bf272daff5dcf39a0bb
Andre Debuisne <andre.debuisne@gmail.com>
2018-10-29 18:54:10 +0100

    Fixing A*

--- src/path_finder.c
+++ src/path_finder.c
@@ -14,14 +14,14 @@ void find_shortest_path(struct node start, struct node finish, struct map *m)
     //int counter = 0;
     while (1)
     {
-        //g_map[current_node.i][current_node.j].type = 'V';
+        g_map[current_node.i][current_node.j].type = '@';
         //current_node.type = 'V';
         if (vector_cmp(current_node.pos, finish.pos))
         {
             break;
         }
         //printf("Current node : (%f;%f)\n", current_node.pos.x, current_node.pos.y);
-        printf("Current node : (%d;%d)\n", current_node.i, current_node.j);
+        //printf("Current node : (%d;%d)\n", current_node.i, current_node.j);
         g_map[current_node.i][current_node.j].open = 2;
         //current_node.open = 2;
 
@@ -41,9 +41,14 @@ void find_shortest_path(struct node start, struct node finish, struct map *m)
                         && current_node.i + a < m->width
                         && current_node.j + b < m->width)
                 {
+                    printf("i :  %d | j = %d\n", current_node.i +a, current_node.j + b);
                     neighbors[index] = g_map[current_node.i + a][current_node.j + b];
-                    g_map[current_node.i + a][current_node.j + b].type = 'V';
-                    if (neighbors[index].type == '#' || neighbors[index].open == 2)
+                    //g_map[current_node.i + a][current_node.j + b].type = 'V';
+                    g_map[current_node.i + a][current_node.j + b].open = 1;
+                    if (neighbors[index].type == '#' || 
+                        neighbors[index].type == '"' || 
+                        neighbors[index].open == 2   || 
+                        neighbors[index].open == 1)
                     {
                         continue;
                     }
@@ -59,9 +64,9 @@ void find_shortest_path(struct node start, struct node finish, struct map *m)
         }
 
         /* TODO: FIND MINIMUM*/ 
-        struct node min_node = find_minimum(neighbors, 8);
-        printf("Current node : (%d; %d)\n", current_node.i, current_node.j);
-        printf("New node : (%d; %d)\n", min_node.i, min_node.j);
+        struct node min_node = find_minimum(neighbors, index);
+        //printf("Current node : (%d; %d)\n", current_node.i, current_node.j);
+        //printf("New node : (%d; %d)\n", min_node.i, min_node.j);
         current_node = g_map[min_node.i][min_node.j];
 
         free(neighbors);
@@ -122,7 +127,6 @@ struct node **map_init(struct map *m)
         }
     }
     g_map = nodes;
-    find_shortest_path(start, finish, m);
 
     for (int i = 0; i < mheight; i++)
     {
@@ -133,5 +137,16 @@ struct node **map_init(struct map *m)
         fprintf(stdout,"\n");
     }
 
+    find_shortest_path(start, finish, m);
+    puts("DISPLAYING SHORTEST PATH:");
+
+    for (int i = 0; i < mheight; i++)
+    {
+        for (int j = 0; j < mwidth; j++)
+        {
+            fprintf(stdout," %c", g_map[i][j].type);
+        }
+        fprintf(stdout,"\n");
+    }
     return nodes;
 }
