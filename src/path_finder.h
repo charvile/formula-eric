39438966b0a3c7aa04a80c12af3b148ef61d4092
Andre Debuisne <andre.debuisne@epita.fr>
2018-10-30 13:55:56 +0100

    Added list structure

--- src/path_finder.h
+++ src/path_finder.h
@@ -2,6 +2,30 @@
 #define PATH_FINDER_H
 
 #include "control.h"
+#include <stddef.h>
+
+struct list_element
+{
+    struct node *node;
+    struct node *next;
+};
+
+struct list_node
+{
+    size_t size;
+    struct node *first;
+};
+
+/* NOTE: list functions*/
+
+struct list_node *init_list(struct list_element *node);
+struct list_node *insert(struct list_node *list, struct node node);
+struct list_node *remove(struct list_node *list, struct node node);
+void destroy(struct list_node *list);
+struct list_element *pop_list(struct list_node *list);
+struct list_element *find_element(struct list_node *list, struct node node);
+
+
 
 struct node
 {
@@ -13,7 +37,7 @@ struct node
     float f_cost;
     float h_cost;
     float g_cost;
-    int i; /* Indexes in the matrix */ 
+    int i; /* Indexes in the matrix */
     int j;
 };
 
