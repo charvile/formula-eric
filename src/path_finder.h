a47eed049dfd55bef8f7e7d56753a41211f123e4
Andre Debuisne <andre.debuisne@gmail.com>
2018-10-30 17:09:29 +0100

    A* optimized

--- src/path_finder.h
+++ src/path_finder.h
@@ -31,7 +31,6 @@ struct node
 {
     char type;
     char open; /* 0 -> not seen | 1 -> open | 2 -> closed */
-    struct node *next; /* Position in matrix of following node*/
     struct node *previous; /* Position in matrix of previous node*/
     struct vector2 pos; /* Position on the map*/
     float f_cost;
